aa763396e82a3c992e11b530c2e36b131df35374
lxd <xdli@thoughtworks.com>
2015-02-15 16:04:47 +0800

    Add several BNF productions and left-recursion-elimination

---
+++ test/test-parser.js
@@ -0,0 +1,19 @@
+var Lexer = require('../lib/lexer');
+var assert  = require('assert');
+var _  = require('underscore');
+var parser = require('../lib/parser');
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('Check BNF table', function(){
+  it('check if any nonterminal underivable', function(){
+    parser.check();
+  })
+})
+
+
+describe('Precompile table', function(){
+  it('elimination left recursion', function(){
+    parser.precompile();
+  })
+})
