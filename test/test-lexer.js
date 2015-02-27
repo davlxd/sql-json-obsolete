5baf7ea826386af333cfe918a10e6d67f4c56023
lxd <xdli@thoughtworks.com>
2015-02-27 17:43:35 +0800

    Rewrite lexer

--- test/test-lexer.js
+++ test/test-lexer.js
@@ -7,12 +7,29 @@ describe('Lexer should return correct tokens', function(){
   it('test case number 1', function(done){
     var input = '   \nfuselect\t selectdb \tfrom\t3.14\n3ck';
     var lexer = new Lexer(input);
-    expect(lexer.next()).to.eql({'tag': 'ID', 'value':'fuselect'});
-    expect(lexer.next()).to.eql({'tag': 'ID', 'value':'selectdb'});
-    expect(lexer.next()).to.eql({'tag': 'FROM'});
-    expect(lexer.next()).to.eql({'tag': 'NUM', 'value':3.14});
-    expect(lexer.next()).to.eql({'tag': 'NUM', 'value':3});
-    expect(lexer.next()).to.eql({'tag': 'ID', 'value':'ck'});
+    expect(lexer.next()).to.eql(['NAME', 'fuselect']);
+    expect(lexer.next()).to.eql(['NAME', 'selectdb']);
+    expect(lexer.next()).to.eql(['FROM']);
+    expect(lexer.next()).to.eql(['NUM', 3.14]);
+    expect(lexer.next()).to.eql(['NUM', 3]);
+    expect(lexer.next()).to.eql(['NAME', 'ck']);
     done();
   })
+
+  it('test case number 2', function(done){
+    var input = 'select * where location like \'earth%\' and msd >= 34.3';
+    var lexer = new Lexer(input);
+    expect(lexer.next()).to.eql(['SELECT']);
+    expect(lexer.next()).to.eql(['*']);
+    expect(lexer.next()).to.eql(['WHERE']);
+    expect(lexer.next()).to.eql(['NAME', 'location']);
+    expect(lexer.next()).to.eql(['LIKE']);
+    expect(lexer.next()).to.eql(['STRING', 'earth%']);
+    expect(lexer.next()).to.eql(['AND']);
+    expect(lexer.next()).to.eql(['NAME', 'msd']);
+    expect(lexer.next()).to.eql(['COMPARISON', '>=']);
+    expect(lexer.next()).to.eql(['NUM', 34.3]);
+    done();
+  })
+
 })
