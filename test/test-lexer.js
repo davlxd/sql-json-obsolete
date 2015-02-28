42efdce10945828a6bcd05cecfcb9aad39cb80a8
lxd <xdli@thoughtworks.com>
2015-02-28 10:54:00 +0800

    Fix bugs

--- test/test-lexer.js
+++ test/test-lexer.js
@@ -17,7 +17,7 @@ describe('Lexer should return correct tokens', function(){
   })
 
   it('test case number 2', function(done){
-    var input = 'select * where location like \'earth%\' and msd >= 34.3';
+    var input = 'select * where location like \'earth%\' and msd >= 34.3;';
     var lexer = new Lexer(input);
     expect(lexer.next()).to.eql(['SELECT']);
     expect(lexer.next()).to.eql(['*']);
@@ -29,7 +29,32 @@ describe('Lexer should return correct tokens', function(){
     expect(lexer.next()).to.eql(['NAME', 'msd']);
     expect(lexer.next()).to.eql(['COMPARISON', '>=']);
     expect(lexer.next()).to.eql(['NUM', 34.3]);
+    expect(lexer.next()).to.eql([';']);
     done();
   })
+
+  it('test case number 3', function(done){
+    var input = 'select min(test) from default insert into x test char go  to int where null';
+    var lexer = new Lexer(input);
+    expect(lexer.next()).to.eql(['SELECT']);
+    expect(lexer.next()).to.eql(['AMMSC', 'MIN']);
+    expect(lexer.next()).to.eql(['(']);
+    expect(lexer.next()).to.eql(['NAME', 'test']);
+    expect(lexer.next()).to.eql([')']);
+    expect(lexer.next()).to.eql(['FROM']);
+    expect(lexer.next()).to.eql(['DEFAULT']);
+    expect(lexer.next()).to.eql(['INSERT']);
+    expect(lexer.next()).to.eql(['INTO']);
+    expect(lexer.next()).to.eql(['NAME', 'x']);
+    expect(lexer.next()).to.eql(['NAME', 'test']);
+    expect(lexer.next()).to.eql(['CHARACTER']);
+    expect(lexer.next()).to.eql(['GOTO']);
+    expect(lexer.next()).to.eql(['INTEGER']);
+    expect(lexer.next()).to.eql(['WHERE']);
+    expect(lexer.next()).to.eql(['NULLX']);
+    done();
+  })
+
+
 
 })
