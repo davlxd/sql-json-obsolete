8643f3d5d2f1ecd1ba1f2689d92f72ac3c79ebe6
lxd <xdli@thoughtworks.com>
2015-03-25 17:57:29 +0800

    Add two more utility funcions

--- test/test-util.js
+++ test/test-util.js
@@ -13,5 +13,33 @@ describe('Util function test', function(){
     arr1[1][0] = 'c';
     expect(util.array2dEqual(arr0, arr1)).to.be.false;
   });
+
+  it('arrayEqual', function(){
+    var arr0 = [ [ [ '$' ] ],
+                 [ [], [ '+' ] ],
+                 [ [ '+', ')' ], [ '+', '*', ')' ] ],
+                 [ [ '+', '*', ')' ] ],
+                 [ [ '+', '*', ')' ] ],
+                 [ [ '+', '*', ')' ] ],
+                 [ [] ],
+                 [ [] ],
+                 [ [], [ ')', '+' ] ],
+                 [ [], [ '*' ] ],
+                 [ [] ],
+                 [ 'a' ],
+                 'b'
+               ];
+    var arr1 = util.cloneArray(arr0);
+    var arr2 = util.cloneArray(arr0);
+    expect(util.arrayEqual(arr0, arr1)).to.be.true;
+    expect(util.arrayEqual(arr0, arr2)).to.be.true;
+
+    arr1[2][0][0] = '-';
+    expect(util.arrayEqual(arr0, arr1)).to.be.false;
+
+    arr2[12] = 'c';
+    expect(util.arrayEqual(arr0, arr1)).to.be.false;
+  });
+
 })
 
